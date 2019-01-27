222bc969c68c9941ab5c2c2a7f267487e3cc914b
TungNQ4 <tungnguyen.dev@gmail.com>
2019-01-27 14:47:56 +0700

    release 1.1.3

--- ios-ci.rb
+++ ios-ci.rb
@@ -1,7 +1,7 @@
 class IosCi < Formula
   desc "Build, Archive & Export iOS/MacOS app via command-line"
   homepage "https://tungdev1209.github.io/ios-ci/"
-  url "https://github.com/tungdev1209/ios-ci/archive/v1.1.2.tar.gz"
+  url "https://github.com/tungdev1209/ios-ci/archive/v1.1.3.tar.gz"
   sha256 "31d8c7313d7f49fe99df0fa390a4496aa9a178e1ad8c12657951e38063796547"
 
   def install
@@ -14,6 +14,6 @@ class IosCi < Formula
   end
 
   test do
-    system "#{bin}/ios-ci", "__test_cmd"
+    system bin/"ios-ci", "-v"
   end
 end
