fa4ab4ba3a478bfeb7434c47a9724ad409615c84
TungNQ4 <tungnguyen.dev@gmail.com>
2019-02-24 16:52:28 +0700

    update version 1.1.9

--- xcode-ci.rb
+++ xcode-ci.rb
@@ -1,7 +1,7 @@
 class XcodeCi < Formula
   desc "CI iOS/MacOS app via command-line"
   homepage "https://tungdev1209.github.io/xcode-ci/"
-  url "https://github.com/tungdev1209/xcode-ci/archive/v1.1.8.tar.gz"
+  url "https://github.com/tungdev1209/xcode-ci/archive/v1.1.9.tar.gz"
   sha256 "6d0ccc27692944384c7c6c64c3c5d64e1eec54236b20009907b3168db7dc730e"
 
   def install
