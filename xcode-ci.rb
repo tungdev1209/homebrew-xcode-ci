65a62e7d2a24c0eea31c917934828ab5d8fecbae
Tung <tung@earable.ai>
2021-02-08 02:05:28 +0700

    release 1.1.10

--- xcode-ci.rb
+++ xcode-ci.rb
@@ -1,7 +1,7 @@
 class XcodeCi < Formula
   desc "CI iOS/MacOS app via command-line"
   homepage "https://tungdev1209.github.io/xcode-ci/"
-  url "https://github.com/tungdev1209/xcode-ci/archive/v1.1.9.tar.gz"
+  url "https://github.com/tungdev1209/xcode-ci/archive/v1.1.10.tar.gz"
   sha256 "c70b992463e3546c6d11cf07359a5da0d4efb51881117f6403f986d0b791088e"
 
   def install
