6d290073e80be13432bf5784def44d2ba20ab0e7
tung <66933071+tung-earable@users.noreply.github.com>
2021-09-08 11:39:00 +0700

    update version 1.1.14

--- xcode-ci.rb
+++ xcode-ci.rb
@@ -1,7 +1,7 @@
 class XcodeCi < Formula
   desc "CI iOS/MacOS app via command-line"
   homepage "https://tungdev1209.github.io/xcode-ci/"
-  url "https://github.com/tungdev1209/xcode-ci/archive/v1.1.13.tar.gz"
+  url "https://github.com/tungdev1209/xcode-ci/archive/v1.1.14.tar.gz"
   sha256 "69903cfc8c587260d1ce5b9fc1d132ca58a60c728de5384a478316979fb7c84f"
 
   def install
