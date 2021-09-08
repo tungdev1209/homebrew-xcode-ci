b506f4d8ade0bcdc5e584ded9043792b80a08835
tung <66933071+tung-earable@users.noreply.github.com>
2021-09-08 11:48:53 +0700

    update sha

--- xcode-ci.rb
+++ xcode-ci.rb
@@ -2,7 +2,7 @@ class XcodeCi < Formula
   desc "CI iOS/MacOS app via command-line"
   homepage "https://tungdev1209.github.io/xcode-ci/"
   url "https://github.com/tungdev1209/xcode-ci/archive/v1.1.14.tar.gz"
-  sha256 "69903cfc8c587260d1ce5b9fc1d132ca58a60c728de5384a478316979fb7c84f"
+  sha256 "f10b3ce0fbe4042a501e5e78ee649f1bd993decf61331a623c13d599cbe95246"
 
   def install
     bin.install "xcode-ci"
